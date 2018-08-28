f4f2da69b2411f9f8e51cd5f5489fa473d834dcc
Jason Nelson <jason@carbonmade.com>
2018-08-28 15:16:43 -0700

    Format FourCC and make readonly

--- MatrixIO.IO.Bmff/IO/Bmff/FourCC.cs
+++ MatrixIO.IO.Bmff/IO/Bmff/FourCC.cs
@@ -3,16 +3,20 @@ using System.Text;
 
 namespace MatrixIO.IO.Bmff
 {
-    public struct FourCC
+    public readonly struct FourCC
     {
-        private uint _FourCC;
+        private readonly uint _FourCC;
 
-        public FourCC(string fourcc) : this(Encoding.UTF8.GetBytes(fourcc)) { }
+        public FourCC(string fourcc)
+            : this(Encoding.UTF8.GetBytes(fourcc)) { }
+
         public FourCC(byte[] fourcc)
         {
             _FourCC = BitConverter.ToUInt32(fourcc, 0).NetworkToHostOrder();
         }
+
         public FourCC(int fourcc) : this((uint)fourcc) { }
+
         [CLSCompliant(false)]
         public FourCC(uint fourcc)
         {
@@ -23,39 +27,42 @@ namespace MatrixIO.IO.Bmff
         {
             return (int)fourcc._FourCC;
         }
+
         public static implicit operator FourCC(int fourcc)
         {
             return new FourCC(fourcc);
         }
+
         public static implicit operator uint(FourCC fourcc)
         {
             return fourcc._FourCC;
         }
+
         public static implicit operator FourCC(uint fourcc)
         {
             return new FourCC(fourcc);
         }
+
         public static implicit operator string(FourCC fourcc)
         {
             return fourcc.ToString();
         }
+
         public static implicit operator FourCC(string fourcc)
         {
             return new FourCC(fourcc);
         }
-        public static implicit operator byte[](FourCC fourcc)
+
+        public static implicit operator byte[] (FourCC fourcc)
         {
             return fourcc.GetBytes();
         }
 
         public static bool operator ==(FourCC a, FourCC b)
         {
-            if (System.Object.ReferenceEquals(a, b)) return true;
-
-            if (((object)a == null) || ((object)b == null)) return false;
-
             return a._FourCC == b._FourCC;
         }
+
         public static bool operator !=(FourCC a, FourCC b)
         {
             return !(a == b);
@@ -63,24 +70,16 @@ namespace MatrixIO.IO.Bmff
 
         public override bool Equals(object obj)
         {
-            if (obj == null) return false;
-            if (obj is FourCC)
-            {
-                if (((FourCC)obj)._FourCC == _FourCC) return true;
-                else return false;
-            }
-            else return false;
+            return obj is FourCC other && this._FourCC == other._FourCC;
         }
 
-        public override int GetHashCode()
-        {
-            return (int)_FourCC;
-        }
+        public override int GetHashCode() => (int)_FourCC;
 
         public byte[] GetBytes()
         {
             return BitConverter.GetBytes(_FourCC.HostToNetworkOrder());
         }
+
         public override string ToString()
         {
             try
@@ -92,7 +91,7 @@ namespace MatrixIO.IO.Bmff
             }
             catch (ArgumentException) { } // UTF8 conversion failed
 
-            return String.Format("0x{0:x8}", _FourCC);
+            return string.Format("0x{0:x8}", _FourCC);
         }
     }
 }
