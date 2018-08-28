4a9a91fd70a443c7cdc211f9f06ccb8d7280c254
Jason Nelson <jason@carbonmade.com>
2018-08-28 15:14:39 -0700

    Format BoxType and make readonly struct

--- MatrixIO.IO.Bmff/IO/Bmff/BoxType.cs
+++ MatrixIO.IO.Bmff/IO/Bmff/BoxType.cs
@@ -1,58 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace MatrixIO.IO.Bmff
 {
-    public struct BoxType
+    public readonly struct BoxType
     {
         private static readonly byte[] BASE_UUID = {
             0x00, 0x00, 0x00, 0x00, // this portion is replaced by the type
-            0x00, 0x11, 0x00, 0x10, 0x80, 0x00, 0x00, 0xAA, 0x00, 0x38, 0x9B, 0x71 
+            0x00, 0x11, 0x00, 0x10, 0x80, 0x00, 0x00, 0xAA, 0x00, 0x38, 0x9B, 0x71
         };
 
         private static readonly FourCC UUID_FOURCC = new FourCC(0x75756964);
 
-        private FourCC _FourCC;
-        public FourCC FourCC { get { return _FourCC; } }
+        // public BoxType(BoxTypes type) : this((uint)type) { }
+        public BoxType(string fourcc) :
+            this(new FourCC(fourcc))
+        { }
 
-        private Guid _UserType;
-        public Guid UserType { get { return _UserType; } }
+        public BoxType(uint fourcc) :
+            this(new FourCC(fourcc))
+        { }
 
-        //public BoxType(BoxTypes type) : this((uint)type) { }
-        public BoxType(string fourcc) : this(new FourCC(fourcc)) { }
-        public BoxType(uint fourcc) : this(new FourCC(fourcc)) {}
         public BoxType(FourCC fourcc)
         {
-            _FourCC = fourcc;
+            FourCC = fourcc;
 
             byte[] uuidBytes = new byte[16];
-            Buffer.BlockCopy(_FourCC.GetBytes(), 0, uuidBytes, 0, 4);
+
+            Buffer.BlockCopy(FourCC.GetBytes(), 0, uuidBytes, 0, 4);
             Buffer.BlockCopy(BASE_UUID, 4, uuidBytes, 4, 12);
-            
-            _UserType = new Guid(uuidBytes);
+
+            UserType = new Guid(uuidBytes);
         }
+
         public BoxType(Guid uuid)
         {
-            _UserType = uuid;
+            UserType = uuid;
 
             bool isBaseType = true;
             byte[] uuidBytes = uuid.ToByteArray();
-            for (int i = 4; i < 16; i++) if (uuidBytes[i] != BASE_UUID[i]) isBaseType=false;
+            for (int i = 4; i < 16; i++) if (uuidBytes[i] != BASE_UUID[i]) isBaseType = false;
 
-            if (isBaseType) _FourCC = new FourCC(BitConverter.ToUInt32(uuidBytes, 0).NetworkToHostOrder());
-            else _FourCC = UUID_FOURCC;
+            if (isBaseType)
+            {
+                FourCC = new FourCC(BitConverter.ToUInt32(uuidBytes, 0).NetworkToHostOrder());
+            }
+
+            else FourCC = UUID_FOURCC;
         }
+
+        public FourCC FourCC { get; }
+
+        public Guid UserType { get; }
 
         public static implicit operator BoxType(FourCC fourcc)
         {
             return new BoxType(fourcc);
         }
+
         public static implicit operator BoxType(string fourcc)
         {
             return new BoxType(fourcc);
         }
+
         public static implicit operator BoxType(uint fourcc)
         {
             return new BoxType(fourcc);
@@ -60,12 +69,9 @@ namespace MatrixIO.IO.Bmff
 
         public static bool operator ==(BoxType a, BoxType b)
         {
-            if (System.Object.ReferenceEquals(a, b)) return true;
-
-            if (((object)a == null) || ((object)b == null)) return false;
-
-            return a._UserType == b._UserType;
+            return a.UserType == b.UserType;
         }
+
         public static bool operator !=(BoxType a, BoxType b)
         {
             return !(a == b);
@@ -73,13 +79,10 @@ namespace MatrixIO.IO.Bmff
 
         public override bool Equals(object obj)
         {
-            return base.Equals(obj) && ((BoxType)obj)._UserType == _UserType;
+            return base.Equals(obj) && ((BoxType)obj).UserType == UserType;
         }
 
-        public override int GetHashCode()
-        {
-            return _UserType.GetHashCode();
-        }
+        public override int GetHashCode() => UserType.GetHashCode();
 
         public override string ToString()
         {
