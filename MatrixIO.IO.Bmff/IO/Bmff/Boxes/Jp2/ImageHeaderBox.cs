3bc380c847e0589489888e370de14a811187e91c
Jason Nelson <jason@carbonmade.com>
2018-09-22 12:01:23 -0700

    Format ImageHeaderBox

--- MatrixIO.IO.Bmff/IO/Bmff/Boxes/Jp2/ImageHeaderBox.cs
+++ MatrixIO.IO.Bmff/IO/Bmff/Boxes/Jp2/ImageHeaderBox.cs
@@ -20,46 +20,66 @@ namespace MatrixIO.IO.Bmff.Boxes
         {
             base.LoadFromStream(stream);
 
-            _Height = stream.ReadBEUInt32();
-            _Width = stream.ReadBEUInt32();
-            _NumberOfComponents = stream.ReadBEUInt16();
+            _height = stream.ReadBEUInt32();
+            _width = stream.ReadBEUInt32();
+            _numberOfComponents = stream.ReadBEUInt16();
             BitsPerComponent = stream.ReadOneByte();
-            _CompressionType = stream.ReadOneByte();
-            _IsUnknownColorspace = stream.ReadOneByte();
-            _IsIntellectualProperty = stream.ReadOneByte();
+            CompressionType = stream.ReadOneByte();
+            _isUnknownColorSpace = stream.ReadOneByte();
+            _isIntellectualProperty = stream.ReadOneByte();
         }
 
         protected override void SaveToStream(Stream stream)
         {
             base.SaveToStream(stream);
 
-            stream.WriteBEUInt32(_Height);
-            stream.WriteBEUInt32(_Width);
-            stream.WriteBEUInt16(_NumberOfComponents);
+            stream.WriteBEUInt32(_height);
+            stream.WriteBEUInt32(_width);
+            stream.WriteBEUInt16(_numberOfComponents);
             stream.WriteOneByte(BitsPerComponent);
-            stream.WriteOneByte(_CompressionType);
-            stream.WriteOneByte(_IsUnknownColorspace);
-            stream.WriteOneByte(_IsIntellectualProperty);
+            stream.WriteOneByte(CompressionType);
+            stream.WriteOneByte(_isUnknownColorSpace);
+            stream.WriteOneByte(_isIntellectualProperty);
         }
 
-        private uint _Height;
-        public long Height { get { return _Height; } set { _Height = checked((uint)value); } }
+        private uint _height;
+        public long Height
+        {
+            get => _height;
+            set => _height = checked((uint)value);
+        }
 
-        private uint _Width;
-        public long Width { get { return _Width; } set { _Width = checked((uint)value); } }
+        private uint _width;
+        public long Width
+        {
+            get => _width;
+            set => _width = checked((uint)value);
+        }
 
-        private ushort _NumberOfComponents;
-        public int NumberOfComponents { get { return _NumberOfComponents; } set { _NumberOfComponents = checked((ushort)value); } }
+        private ushort _numberOfComponents;
+        public int NumberOfComponents
+        {
+            get => _numberOfComponents; set => 
+                _numberOfComponents = checked((ushort)value);
+        }
 
         public byte BitsPerComponent { get; set; }
 
-        private byte _CompressionType = 7; // Default value defined in 15444-1 Annex I
-        public byte CompressionType { get { return _CompressionType; } set { _CompressionType = value; } }
+        // Note: default value defined in 15444-1 Annex I
+        public byte CompressionType { get; set; } = 7;
 
-        private byte _IsUnknownColorspace;
-        public bool IsUnknownColorspace { get { return _IsUnknownColorspace > 0; } set { _IsUnknownColorspace = value ? (byte)0x01 : (byte)0x00; } }
+        private byte _isUnknownColorSpace;
+        public bool IsUnknownColorspace
+        {
+            get => _isUnknownColorSpace > 0;
+            set => _isUnknownColorSpace = value ? (byte)0x01 : (byte)0x00;
+        }
 
-        private byte _IsIntellectualProperty;
-        public bool IsIntellectualProperty { get { return _IsIntellectualProperty > 0; } set { _IsIntellectualProperty = value ? (byte)0x01 : (byte)0x00; } }
+        private byte _isIntellectualProperty;
+        public bool IsIntellectualProperty
+        {
+            get => _isIntellectualProperty > 0;
+            set => _isIntellectualProperty = value ? (byte)0x01 : (byte)0x00;
+        }
     }
 }
