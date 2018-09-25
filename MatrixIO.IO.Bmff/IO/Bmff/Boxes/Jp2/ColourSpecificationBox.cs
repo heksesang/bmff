18515b7e45164ff7d00131677d0c1b9d2730fe13
Jason Nelson <jason@carbonmade.com>
2018-09-24 17:58:56 -0700

    Format ColourSpecificationBox

--- MatrixIO.IO.Bmff/IO/Bmff/Boxes/Jp2/ColourSpecificationBox.cs
+++ MatrixIO.IO.Bmff/IO/Bmff/Boxes/Jp2/ColourSpecificationBox.cs
@@ -8,13 +8,29 @@ namespace MatrixIO.IO.Bmff.Boxes
     [Box("colr", "Color Specification Box")]
     public sealed class ColourSpecificationBox : Box, IContentBox
     {
-        public ColourSpecificationBox() : base() { }
-        public ColourSpecificationBox(Stream stream) : base(stream) { }
+        public ColourSpecificationBox() 
+            : base() { }
+
+        public ColourSpecificationBox(Stream stream) 
+            : base(stream) { }
+
+        public SpecificationMethods SpecificationMethod { get; set; }
+
+        public byte Precedence { get; set; }
+
+        public byte ColorspaceApproximation { get; set; }
+
+        public EnumeratedColorspaces EnumeratedColorspace { get; set; }
 
         internal override ulong CalculateSize()
         {
             ulong size = base.CalculateSize() + 1 + 1 + 1;
-            if (SpecificationMethod == SpecificationMethods.EnumeratedColorspace) size += 4;
+
+            if (SpecificationMethod == SpecificationMethods.EnumeratedColorspace)
+            {
+                size += 4;
+            }
+
             return size;
         }
 
@@ -26,7 +42,10 @@ namespace MatrixIO.IO.Bmff.Boxes
             Precedence = stream.ReadOneByte();
             ColorspaceApproximation = stream.ReadOneByte();
             if (SpecificationMethod == SpecificationMethods.EnumeratedColorspace)
+            {
                 EnumeratedColorspace = (EnumeratedColorspaces)stream.ReadBEUInt32();
+            }
+
             // TODO: Read Profile?
         }
 
@@ -37,18 +56,14 @@ namespace MatrixIO.IO.Bmff.Boxes
             stream.WriteOneByte((byte)SpecificationMethod);
             stream.WriteOneByte(Precedence);
             stream.WriteOneByte(ColorspaceApproximation);
+
             if (SpecificationMethod == SpecificationMethods.EnumeratedColorspace)
+            {
                 stream.WriteBEUInt32((uint)EnumeratedColorspace);
+            }
+
             // TODO: Write Profile
         }
-
-        public SpecificationMethods SpecificationMethod { get; set; }
-
-        public byte Precedence { get; set; }
-
-        public byte ColorspaceApproximation { get; set; }
-
-        public EnumeratedColorspaces EnumeratedColorspace { get; set; }
 
         public enum SpecificationMethods : byte
         {
