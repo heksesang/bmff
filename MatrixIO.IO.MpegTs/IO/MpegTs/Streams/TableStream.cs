7e2f3da4d2550c38cf3c786c7ac692694271f68b
Jason Nelson <jason@carbonmade.com>
2018-09-26 08:54:56 -0700

    Format TableStream

--- MatrixIO.IO.MpegTs/IO/MpegTs/Streams/TableStream.cs
+++ MatrixIO.IO.MpegTs/IO/MpegTs/Streams/TableStream.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using MatrixIO.IO.MpegTs.Tables;
 
 namespace MatrixIO.IO.MpegTs.Streams
 {
-    public class TableStream : TsStream<TsTable>
+    public sealed class TableStream : TsStream<TsTable>
     {
         protected override TsTable ProcessUnit(TsUnit unit)
         {
