cfad876d4c33c3767b8c1ea13bf8332d40222d9e
Jason Nelson <jason@carbonmade.com>
2018-09-26 09:15:04 -0700

    Breakout ProgramAssociation

---
+++ MatrixIO.IO.MpegTs/IO/MpegTs/Tables/ProgramAssociation.cs
@@ -0,0 +1,19 @@
+﻿namespace MatrixIO.IO.MpegTs.Tables
+{
+    public readonly struct ProgramAssociation
+    {
+        public ProgramAssociation(byte[] buffer, int offset)
+        {
+            int position = offset;
+            ProgramNumber = (ushort)((buffer[position++] << 8) | buffer[position++]);
+            PacketIdentifier = (ushort)(((buffer[position++] << 8) | buffer[position]) & 0x1FFF);
+        }
+
+        public int Length => 2 + 2;
+
+        public ushort ProgramNumber { get; }
+
+        public ushort PacketIdentifier { get; }
+
+    }
+}
