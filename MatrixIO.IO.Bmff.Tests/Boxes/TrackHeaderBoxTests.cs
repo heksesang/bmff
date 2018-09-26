b582f35544368d99b2be7c907eef14460088f525
Jason Nelson <jason@carbonmade.com>
2018-09-26 08:44:02 -0700

    Add TrackHeaderBoxTests

---
+++ MatrixIO.IO.Bmff.Tests/Boxes/TrackHeaderBoxTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Xunit;
+
+namespace MatrixIO.IO.Bmff.Boxes.Tests
+{
+    public class TrackHeaderBoxTests
+    {
+        [Fact]
+        public void CanDecode()
+        {
+            var data = Convert.FromBase64String("AAAAXHRraGQAAAADAAAAAAAAAAAAAAABAAAAAAAABkAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAABAAAAAAyAAAAGaAAA=");
+
+            var box = (TrackHeaderBox)TestHelper.Decode(data);
+
+            Assert.Equal(92u,    box.Size);
+            Assert.Equal(0,      box.Version);
+            Assert.Equal(1u,     box.TrackID);
+            Assert.Equal(1600ul, box.Duration);
+            Assert.Equal(0,      box.Layer);
+            Assert.Equal(0,      box.AlternateGroup);
+            Assert.Equal(0,      box.Volume);
+
+            Assert.True(box.Flags[16]);
+            Assert.True(box.Flags[17]);
+
+            // Ensure it reencodes to the same bytes
+            Assert.True(data.AsSpan().SequenceEqual(TestHelper.Encode(box)));
+        }
+    }
+}
