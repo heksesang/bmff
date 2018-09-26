14e65f8e2e9518fce117419f5d7e082d5b2ec511
Jason Nelson <jason@carbonmade.com>
2018-09-26 09:12:10 -0700

    Breakout AACSCopyControl

---
+++ MatrixIO.IO.MpegTs/IO/MpegTs/AACSCopyControl.cs
@@ -0,0 +1,11 @@
+﻿namespace MatrixIO.IO.MpegTs
+{
+    public enum AACSCopyControl
+    {
+        // TODO: Figure out what these mean from the AACS spec
+        Unencrypted,
+        Unknown1,
+        Unknown2,
+        Unknown3,
+    }
+}
--- MatrixIO.IO.MpegTs/IO/MpegTs/AacsPacket.cs
+++ MatrixIO.IO.MpegTs/IO/MpegTs/AacsPacket.cs
@@ -2,15 +2,6 @@
 
 namespace MatrixIO.IO.MpegTs
 {
-    public enum AACSCopyControl
-    {
-        // TODO: Figure out what these mean from the AACS spec
-        Unencrypted,
-        Unknown1,
-        Unknown2,
-        Unknown3,
-    }
-
     public class AacsPacket
     {
         public const int Length = 4 + 188;
