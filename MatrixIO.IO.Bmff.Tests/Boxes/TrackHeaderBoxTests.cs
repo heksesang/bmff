878d40a9126fe69315dfbf91faaa8c03143f4804
Jason Nelson <jason@carbonmade.com>
2018-12-21 19:12:33 -0800

    Format tests

--- MatrixIO.IO.Bmff.Tests/Boxes/TrackHeaderBoxTests.cs
+++ MatrixIO.IO.Bmff.Tests/Boxes/TrackHeaderBoxTests.cs
@@ -13,13 +13,18 @@ namespace MatrixIO.IO.Bmff.Boxes.Tests
 
             var box = (TrackHeaderBox)TestHelper.Decode(data);
 
-            Assert.Equal(92u,    box.Size);
-            Assert.Equal(0,      box.Version);
-            Assert.Equal(1u,     box.TrackID);
-            Assert.Equal(1600ul, box.Duration);
-            Assert.Equal(0,      box.Layer);
-            Assert.Equal(0,      box.AlternateGroup);
-            Assert.Equal(0,      box.Volume);
+            Assert.Equal(92u,       box.Size);
+            Assert.Equal(0,         box.Version);
+            Assert.Equal(1u,        box.TrackID);
+            Assert.Equal(1600ul,    box.Duration);
+            Assert.Equal(0,         box.Layer);
+            Assert.Equal(0,         box.AlternateGroup);
+            Assert.Equal(0,         box.Volume);
+            Assert.Equal(52428800u, box.Width.Value);
+            Assert.Equal(26869760u, box.Height.Value);
+
+            Assert.Equal(800, box.Width.ToDouble());
+            Assert.Equal(410, box.Height.ToDouble());
 
             Assert.True(box.Flags[16]);
             Assert.True(box.Flags[17]);
